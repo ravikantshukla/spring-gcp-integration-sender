cfdb1367309bd003be4b62a491d0bef68cbf258a
Ravi Kant Shukla <ravikanth.s@sleepcountry.ca>
2023-01-10 18:25:58 +0530

    Initial commit

---
+++ src/main/java/com/spring/gcp/integration/sender/SpringGcpIntegrationSenderApplication.java
@@ -0,0 +1,13 @@
+package com.spring.gcp.integration.sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGcpIntegrationSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGcpIntegrationSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/gcp/integration/sender/SpringGcpIntegrationSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.gcp.integration.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGcpIntegrationSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
